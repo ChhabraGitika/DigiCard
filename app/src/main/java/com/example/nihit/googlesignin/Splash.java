49f46fe444fe86d8879dff8094203cfcb8fec1c4
Neha Goyal <neha18042@iiitd.ac.in>
2018-11-30 16:55:53 +0530

    Direct swipe

--- app/src/main/java/com/example/nihit/googlesignin/Splash.java
+++ app/src/main/java/com/example/nihit/googlesignin/Splash.java
@@ -6,17 +6,34 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Splash extends Activity {
+    GoogleSignInClient mGoogleSignInClient;
+    FirebaseAuth mAuth;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        mAuth=FirebaseAuth.getInstance();
         int secondsDelayed = 1;
-        new Handler().postDelayed(new Runnable() {
-            public void run() {
-                startActivity(new Intent(Splash.this, MainActivity.class));
-                finish();
-            }
-        }, secondsDelayed * 1000);
+        if (mAuth.getCurrentUser() == null) {
+            new Handler().postDelayed(new Runnable() {
+                public void run() {
+                    startActivity(new Intent(Splash.this, MainActivity.class));
+                    finish();
+                }
+            }, secondsDelayed * 1000);
+        }
+
+    }
+    protected void onStart(){
+        super.onStart();
+        if (mAuth.getCurrentUser() != null) {
+            finish();
+            startActivity(new Intent(this, Swipe.class));
+        }
     }
 }
